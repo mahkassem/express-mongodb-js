2ebf25f5dcb4cadb03e052a11da9d8ba883c5f17
Mahmoud Kassem <mahmoudalikassem@outlook.com>
2024-05-19 21:00:37 +0300

    node cache

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -5,10 +5,19 @@ import {
   updateCustomer,
   deleteCustomer
 } from '../models/customer.model.js';
+import cache from '../cache.js';
 
 export const getAll = async (req, res) => {
   try {
+    // Check if the data is already in the cache
+    const data = cache.get('customers');
+    if (data) {
+      return res.json(data);
+    }
+    // If not, fetch the data from the database
     const customers = await getCustomers();
+    // Store the data in the cache for 5 minutes
+    cache.set('customers', customers, 600);
     res.json(customers);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -29,7 +38,8 @@ export const create = async (req, res) => {
   try {
     const customer = req.body;
     const data = await createCustomer(customer);
-
+    cache.del('customers');
+    // Return the newly created customer
     res.status(201).json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -41,7 +51,8 @@ export const update = async (req, res) => {
     const { id } = req.params;
     const customer = req.body;
     const data = await updateCustomer(id, customer);
-
+    // Remove the customers key from the cache
+    cache.del('customers');
     res.json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -52,7 +63,8 @@ export const remove = async (req, res) => {
   try {
     const { id } = req.params;
     const data = await deleteCustomer(id);
-
+    // Remove the customers key from the cache
+    cache.del('customers');
     res.json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
